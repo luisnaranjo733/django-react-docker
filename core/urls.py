6c173e34238a3ad49e1f4d7ad4fab5d2d867d83b
Luis Naranjo <luisnaranjo733@gmail.com>
2017-04-02 02:02:49 -0700

    add listing page

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^volunteer_listing/$', views.volunteer_listing, name='volunteer_listing')
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'core/volunteer.html')
+
+def volunteer_listing(request):
+    return render(request, 'core/listing.html')
