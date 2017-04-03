643c5034763ff9a4113dde8d184fe63cf5fbbadf
Luis Naranjo <luisnaranjo733@gmail.com>
2017-04-02 20:14:49 -0700

    Survey looks good!

--- core/views.py
+++ core/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from core.models import Opportunity
+from nwirp.settings import DEBUG
 
 def index(request):
     '''Volunteer home page
     Most likely this page won't be included in production, as NWIRP can hopefully redirect
     directly to the volunteer listing page'''
-    return render(request, 'core/volunteer.html')
+    return render(request, 'core/index.html')
 
 def volunteer_listing(request):
     '''Volunteer opportunity listing page'''
@@ -17,12 +18,22 @@ def volunteer_listing(request):
 
 def survey(request):
     '''Volunteer interest survey page'''
-    params = {}
+    params = {
+        'method': request.method,
+    }
     if request.method == 'POST':
         choices = request.POST.getlist('categories[]')
-        params['opportunities'] = []
+        params['opportunity_list'] = []
+        params['survey_list'] = []
         for opportunity_id in choices:
-            params['opportunities'].append(get_object_or_404(Opportunity, pk=opportunity_id))
+            opportunity = get_object_or_404(Opportunity, pk=opportunity_id)
+            params['opportunity_list'].append(opportunity)
+            for survey in opportunity.surveys.all():
+                if survey not in params['survey_list']:
+                    params['survey_list'].append(survey)
+
+    elif DEBUG: # load all objects on GET requests for debugging purposes
+        params['opportunity_list'] = Opportunity.objects.all()
 
     return render(request, 'core/survey.html', params)
     
