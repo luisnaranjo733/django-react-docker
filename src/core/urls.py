fa65b72cd596313d919965ea783eb3ac51313de1
Luis Naranjo <luisnaranjo733@gmail.com>
2017-07-04 14:22:52 -0700

    bootstrap new react app

--- src/core/urls.py
+++ src/core/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    # url(r'^$', views.volunteer_listing, name='volunteer_listing'),
+    # url(r'^$', views.index, name='index'),
+    url(r'^$', views.volunteer_listing, name='volunteer_listing'),
     url(r'^survey/$', views.survey_page, name='volunteer_survey'),
     url(r'^done/$', views.done, name='survey_complete'),
     url('^reach-out/$', views.reach_out, name='manager-contacts')
